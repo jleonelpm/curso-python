7b2b344079404e998bbdf9788b0b08b26000d2b1
Leonel Pech <jleonelpm@hotmail.com>
2022-05-09 20:11:41 -0500

    Archivos eliminados

--- 11.lectura-archivos/archivos-read1.py
+++
@@ -1,6 +0,0 @@
-# se abre el archivo tzop.txt en modo lectura, devolviéndolo como un objeto de archivo
-# rt es read and text
-stream = open("archivo.txt", "rt", encoding = "utf-8") 
-
-# se imprime el contenido del archivo
-print(stream.read()) 
